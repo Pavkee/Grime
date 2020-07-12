d1e060b081f362cff87dfc1036e37ab71da8fb9b
Pavkee <68195360+Pavkee@users.noreply.github.com>
2020-07-12 22:30:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,34 +11,6 @@ const PREFIX = isRunningLocaly ? '-' : '+'
 
 server.listen(PORT)
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (/d[o0]+g/i.test(msg.content)) {
-    msg.react('709181387579850813')
-  }
-  if (msg.author.bot) {
-    return
-  }
-  
-})
-
-
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const express = require('express')
-const http = require('http')
-const PORT   = process.env.PORT || 3000
-const app = express()
-const server = http.createServer(app)
-const isRunningLocaly = require('os').hostname() === 'DESKTOP-EHM4SR0'
-const PREFIX = isRunningLocaly ? '-' : '+'
-
-server.listen(PORT)
-
 app.get('/', (req, res) => {
   res.send('Placeholder!')
 })
@@ -60,6 +32,9 @@ client.on('message', msg => {
 
 
 client.login(process.env.TOKEN)
+
+
+
 
 
 
