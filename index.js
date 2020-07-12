d6374e8cfa2f6c3f1ef2662a53936a189a591eec
Pavkee <68195360+Pavkee@users.noreply.github.com>
2020-07-12 21:57:26 +0200

    asd

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ".";
+const express = require('express')
+const http = require('http')
+const PORT   = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+const isRunningLocaly = require('os').hostname() === 'DESKTOP-EHM4SR0'
+const PREFIX = isRunningLocaly ? '-' : '+'
+
+server.listen(PORT)
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (/d[o0]+g/.test(msg.content)) {
+  if (/d[o0]+g/i.test(msg.content)) {
     msg.react('709181387579850813')
   }
   if (msg.author.bot) {
@@ -17,5 +27,39 @@ client.on('message', msg => {
 
 
 
-client.login('NzMxMTQzMDAyNTc5MjcxNzcw.XwhwpA.d7SR8m9Xqp6Wseex9finMGqF_vk');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const express = require('express')
+const http = require('http')
+const PORT   = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+const isRunningLocaly = require('os').hostname() === 'DESKTOP-EHM4SR0'
+const PREFIX = isRunningLocaly ? '-' : '+'
+
+server.listen(PORT)
+
+app.get('/', (req, res) => {
+  res.send('Placeholder!')
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (/d[o0]+g/i.test(msg.content)) {
+    msg.react('709181387579850813')
+  }
+  if (msg.author.bot) {
+    return
+  }
+  
+})
+
+
+
+client.login(process.env.TOKEN)
+
+
 
